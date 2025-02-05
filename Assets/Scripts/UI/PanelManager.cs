c15e4011a9570294b3a620d1d33175767eae6021
99jamin <99jamin56@gmail.com>
2025-02-05 17:57:32 +0900

    practice

--- Assets/Scripts/UI/PanelManager.cs
+++ Assets/Scripts/UI/PanelManager.cs
@@ -7,13 +7,19 @@ public class PanelManager : MonoBehaviour
     [SerializeField] private PanelController startPanelController;
 
     public enum PanelType { StartPanel, WinPanel, DrawPanel, LosePanel }
+    private PanelController _currentPanelController;
     
+    
+    /// <summary>
+    /// 표시할 패널 정보 전달하는 함수
+    /// </summary>
+    /// <param name="panelType"></param>
     public void ShowPanel(PanelType panelType)
     {
         switch (panelType)
         {
             case PanelType.StartPanel:
-                
+                ShowPanelController(startPanelController);
                 break;
             case PanelType.WinPanel:
                 
@@ -25,5 +31,15 @@ public class PanelManager : MonoBehaviour
                 
                 break;
         }
+    }
+    
+    private void ShowPanelController(PanelController panelController)
+    {
+        if (_currentPanelController != null)
+        {
+            _currentPanelController.Hide();
+        }
+        panelController.Show();
+        _currentPanelController = panelController;
     }
 }
